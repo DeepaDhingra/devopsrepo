c64569079d68cd109388ed47f2011be1ff486a86
deepa.dhingra <deepa.dhingra@servicenow.com>
2018-09-17 13:37:33 -0700

    first push to personal repo

---
+++ seleniumresultapp/demo/test/com/devopsmaniac/mania/RaghuTest.java
@@ -0,0 +1,27 @@
+package com.devopsmaniac.mania;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Ignore;
+
+public class RaghuTest {
+
+    /*@Test
+    public void somethingRandom() {
+        assertEquals( 3, Demo.somethingRandom() );
+    } */
+
+  /*  @Test
+    @Ignore
+    public void ignoreMePlease() {
+        assertEquals( 1, Demo.somethingRandom() );
+        System.out.println("Hello Testing world");
+    }*/
+
+@Test
+    public void somethingRandom() {
+        assertEquals( 5, 5 );
+    }
+}
